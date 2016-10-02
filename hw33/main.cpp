ffa6ef9cb1db54b56b864bd016607f68a35ca2f9
Alejandro Ramirez Jasso <Alejandro@lauras-mbp.attlocal.net>
2016-10-01 21:15:21 -0700

    Hw 3 Ex has been cleaned up and pointers deleted and set to nullptr

--- hw33/main.cpp
+++ hw33/main.cpp
@@ -1,57 +1,138 @@
-//
-//  main.cpp
-//  hw33
-//
-//  Created by Alejandro Ramirez Jasso on 9/26/16.
-//  Copyright © 2016 Alejandro Ramirez Jasso. All rights reserved.
-//
+/**
+ * Program Name: cis25Fal2016AlejandroRHw3Ex1.cpp
+ * Discussion:   Homework #2
+ * Written By:   Alejandro R
+ * Due Date:     2016/10/04
+ */
 
 #include <iostream>
 using namespace std;
 
 
-//void menu3AlejandroRamirez(void):
-//int* extractUncommonDigitArrayAlejandroR(int*, int);
+void menu03AlejandroRamirez(void);
+void setUpExtractUncommonDigit(void);
 int** setUp2D(int);
 void extractDigitInfo(int* dataAry, int size, int**);
 int* extractUncommonDigit(int**, int);
 
 int main() {
-    int** iPtrPtr = nullptr;
-    int* dataAry = nullptr;
-    int* uncommonAry;
-    int size;
-    int i, j;
     
-    cout << "\nHow many elements? ";
-    cin >> size;
-    
-    dataAry = new int[size];
-    for (i = 0; i < size; i++) {
-        cout << "\nEnter value for element of index " << i << " : ";
-        cin >> *(dataAry + i);
-    }
-    
-    iPtrPtr = setUp2D(size);
-    
-    extractDigitInfo(dataAry, size, iPtrPtr);
-    
-    uncommonAry = extractUncommonDigit(iPtrPtr, size);
-    
-    for (i = 0; i < (*(uncommonAry + 0) * 2 + 1); i++){
-        cout << "\n Index: " << i << " is " << *(uncommonAry + i);
-    }
-    //for (i = 0; i < size; i++) {
-    //    for (j = 0; j < 10; j++) {
-    //    cout <<"\nElement of index " << j << " : "
-    //    << *(*(iPtrPtr + i) + j) << endl;
-    //    }
-    //}
+    menu03AlejandroRamirez();
     
     return 0;
 }
 
-//	void menu3AlejandroRamirez() {}
+
+void menu03AlejandroRamirez() {
+    int option;
+    
+    do {
+        cout << "\n\n**********************************************"
+        "\n*           MENU - Homework #2                 *"
+        "\n* (1) Calling extractUncommonDigitAlejandroR() *"
+        "\n* (2) Quit                                     *"
+        "\n************************************************";
+        
+        cout << "\nSelect an option (1 or 2): ";
+        cin >> option;
+        
+        switch (option) {
+            case 1:
+                setUpExtractUncommonDigit();
+                
+                break;
+                
+            case 2:
+                cout << "\nFun exercise ..." << endl;
+                
+                break;
+                
+            default:
+                
+                cout << "\n  WRONG OPTION!\n";
+        }
+        
+    } while (option != 2);
+}
+
+void setUpExtractUncommonDigit() {
+    int size;
+    int* dataAry = nullptr;
+    int** iPtrPtrAry = nullptr;
+    int* uncommonAry = nullptr;
+    int i;
+    
+    cout << "\nHow many integers? ";
+    cin >> size;
+    
+    dataAry = new int[size];
+    
+    for (i = 0; i < size; i++) {
+        cout << "    Enter integer #" << i + 1 << ": ";
+        cin >> *(dataAry + i);
+    }
+    
+    cout << "\nThe original array:";
+    for (i = 0; i < size; i++) {
+        cout << "\n  " << *(dataAry + i);
+    }
+    
+    cout << "\n\nCalling extractUncommonDigitAlejandroR() -" << endl;
+    
+    iPtrPtrAry = setUp2D(size);
+    
+    extractDigitInfo(dataAry, size, iPtrPtrAry);
+    
+    uncommonAry = extractUncommonDigit(iPtrPtrAry, size);
+    
+    for (i = 0; i < (*(uncommonAry + 0) * 2 + 1); i++){
+        cout << "\n Index: " << i << " is " << *(uncommonAry + i);
+    }
+    
+    delete[] dataAry;
+    dataAry = nullptr;
+    
+    for (i = 0; i < size; i++) {
+        delete[] *(iPtrPtrAry + i);
+        *(iPtrPtrAry + i) = nullptr;
+    }
+    delete[] iPtrPtrAry;
+    iPtrPtrAry = nullptr;
+    
+    delete[] uncommonAry;
+    uncommonAry = nullptr;
+    
+
+}
+
+
+int** setUp2D(int size) {     //addd a ptrtoptr in arg list
+    int** iPtrPtr = nullptr;
+    int i;
+    
+    iPtrPtr = new int*[size];   //iPtrPtr: ptr to #size ptrs to ints
+    
+    for (i = 0; i <size; i++) {
+        *(iPtrPtr + i) = new int[10]();
+    }
+    
+    return iPtrPtr;
+}
+
+void extractDigitInfo(int* dataAry, int size, int** digitInfoAry) {
+    int tmp;
+    int i;
+    
+    for (i = 0; i < size; i++) {
+        tmp = (*(dataAry + i) < 0) ? -(*(dataAry + i)) : *(dataAry + i);
+        
+        do {
+            *(*(digitInfoAry + i) + tmp % 10) = 1;
+            
+            tmp /= 10;
+        } while (tmp != 0);
+    }
+}
 
 int* extractUncommonDigit(int** iPtrPtr, int size) {
     int* allDigitOcc = new int[20]();
@@ -101,36 +182,4 @@ int* extractUncommonDigit(int** iPtrPtr, int size) {
     }
     
     return uncommAry;
-}
-
-void extractDigitInfo(int* dataAry, int size, int** digitInfoAry) {
-    int tmp;
-    int i, j;
-    
-    for (i = 0; i < size; i++) {
-        tmp = (*(dataAry + i) < 0) ? -(*(dataAry + i)) : *(dataAry + i);
-        
-        do {
-            *(*(digitInfoAry + i) + tmp % 10) = 1;
-
-            tmp /= 10;
-        } while (tmp != 0);
-    }
-}
-
-int** setUp2D(int size) {
-    int** iPtrPtr = nullptr;
-    //int size;
-    int i;
-             
-    //1.cout << "\nHow many int's? ";
-    //1.cin >> size;
-    
-    iPtrPtr = new int*[size];   //iPtrPtr: ptr to #size ptrs to ints
-    
-    for (i = 0; i <size; i++) {
-        *(iPtrPtr + i) = new int[10]();
-    }
-             
-    return iPtrPtr;
 }
